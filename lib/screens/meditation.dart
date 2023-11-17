b7ce5ea06b7f40e05e97e408c6971c7cb02d7e1f
Hisnaider Campello <hisnaidercampello@hotmail.com.br>
2023-11-17 12:00:12 -0300

    _AudioSelector widget created in meditaion.dart

--- lib/screens/meditation.dart
+++ lib/screens/meditation.dart
@@ -5,8 +5,89 @@ class MeditationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: BoxConstraints.expand(height: 250),
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "Meditar",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [_AudioSelector()],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _AudioSelector extends StatefulWidget {
+  const _AudioSelector({super.key});
+
+  @override
+  State<_AudioSelector> createState() => __AudioSelectorState();
+}
+
+class __AudioSelectorState extends State<_AudioSelector> {
+  bool isPlaying = false;
+
+  void playAndPause() {
+    if (isPlaying) {
+      ///TODO: pause function
+    } else {
+      ///TODO: play function
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Áudio para meditar",
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          const SizedBox(height: 5),
+          Container(
+            padding: const EdgeInsets.all(7),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Theme.of(context).colorScheme.surface),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: playAndPause,
+                  child: Icon(
+                    isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 18,
+                  ),
+                ),
+                const SizedBox(width: 7),
+                const Expanded(child: Text("teste")),
+                const SizedBox(width: 7),
+                const Icon(
+                  Icons.menu,
+                  size: 18,
+                  color: Colors.grey,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
